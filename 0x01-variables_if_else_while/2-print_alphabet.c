5c6efcfb21cadbc9da3866358aaa7de653c61e7f
Zola123456 <emiledegbe2000@gmail.com>
2022-12-10 09:41:51 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
@@ -18,4 +18,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
+i
