3229ae2a5444300be28ec54006c5dc9383a7d5cb
Zola123456 <emiledegbe2000@gmail.com>
2022-12-16 08:52:48 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
-#include "main.h""
+#include "main.h"
 /**
  * print_numbers - print numbers
  * Return: Always 0.
  */
+
 void print_numbers(void)
 {
 	int i;
 
 	i = 0;
-	while (i < 10)
+	while (1 < 10)
 	{
-		putchar (i + '0');
-			i++;
+		_putchar (i + '0');
+		i++;
 	}
 
 	_putchar ('\n');
