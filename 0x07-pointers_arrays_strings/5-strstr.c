3e04b43b32b649e5632a8aa0684e59cc8228cc77
Zola123456 <emiledegbe2000@gmail.com>
2023-01-02 08:27:28 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strstr _ first occurrence of the substring needle in the string haystack
+ * _strstr - first occurrence of the substring needle in the string haystack
  * @haystack: main str to be examined
  * @needle: searched in haystack
  * Return: return 0
