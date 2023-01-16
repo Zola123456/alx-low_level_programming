abbe6ff071c2c99c8ad641f5412b374f3012d932
Zola123456 <emiledegbe2000@gmail.com>
2023-01-16 08:22:45 -0800

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define STRTYP
 /**
  * struct dog - structure of the dog.
- * @naame: name of the dog.
+ * @name: name of the dog.
  * @age: age of dog.
  * @owner: Owner of dog.
  */
@@ -13,9 +13,9 @@ struct dog
 	char *owner;
 };
 
-typedef struct dog dog_t
+typedef struct dog dog_t;
 
-void init_dog(struct dog *d char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
