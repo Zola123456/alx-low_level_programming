cae28e094e39aab375d7f65e3f77306ad2b198b3
Zola123456 <emiledegbe2000@gmail.com>
2023-01-18 12:00:20 -0800

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,11 +15,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		if (size <= 0)
 		{
-			return(res);
+			return (res);
 		}
 		for (i = 0; i < size; i++)
 		{
-			cmp(array [i]);
+			cmp(array[i]);
 			if (cmp(array[i]) > 0)
 			{
 				res = 1;
