302a1d5cf8ce06cff845b7337c789f308baa0858
Zola123456 <emiledegbe2000@gmail.com>
2023-01-30 17:21:25 -0800

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees
+ *
+ * @head: head
+ * Return: no return
+ */
+
+void free_listint2(listint_t **head)
+{
+
+	listint_t *temp;
+	listint_t *curr;
+
+	if (head != NULL)
+	{
+	curr = *head;
+	while ((temp = curr) != NULL)
+	{
+	curr = curr->next;
+	free(temp);
+	}
+	*head = NULL;
+	}
+}
