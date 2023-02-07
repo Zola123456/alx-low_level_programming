bb82a1fa7873fbd2ae6095f860b31ad8c3c4c04c
Zola123456 <emiledegbe2000@gmail.com>
2023-02-06 16:35:51 -0800

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, return 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filname)
+		return (0);
+	fd = open(filname, o_RDONLY);
+
+	if (fd == -1)
+		return (0);
+}
